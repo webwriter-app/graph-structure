948d0d6fe3e22b7a6505a4fcd56ac2b95715202c
Ferdinand Baumgarten <ferdinand.baumgarten@rwth-aachen.de>
2022-11-08 02:07:17 +0100

    dijkstra

---
+++ algorithms/dijkstra.ts
@@ -0,0 +1,55 @@
+export const dijkstra = (start, graph) => {
+  var dist = {};
+  var prev = {};
+
+  var neighbors = {};
+
+  var q = [];
+
+  for (var node of graph.nodes) {
+    dist[node.name] = Infinity;
+    prev[node.name] = [];
+    neighbors[node.name] = [];
+    q.push(node);
+  }
+
+  for (var link of graph.links) {
+    neighbors[link.source.name].push({
+      name: link.target.name,
+      weight: link.weight,
+    });
+    neighbors[link.target.name].push({
+      name: link.source.name,
+      weight: link.weight,
+    });
+  }
+
+  dist[start.name] = 0;
+
+  while (q.length > 0) {
+    var u = getNodeWithLowestDist(q, dist);
+    q = q.filter((e) => e.name !== u.name);
+
+    for (var n of neighbors[u.name]) {
+      var alt = dist[u.name] + n.weight;
+      if (alt < dist[n.name]) {
+        dist[n.name] = alt;
+        prev[n.name] = u.name;
+      }
+    }
+  }
+
+  console.log(dist);
+  console.log(prev);
+};
+
+function getNodeWithLowestDist(q, dist) {
+  var currentMin = q[0];
+  for (var node of q) {
+    if (dist[node.name] < dist[currentMin.name]) {
+      currentMin = node;
+    }
+  }
+
+  return currentMin;
+}
--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ import { LitElementWw } from "@webwriter/lit";
 import { html } from "lit";
 import { customElement } from "lit/decorators.js";
 import "./components/dfs_component.ts";
+import "./components/dijkstra_component.ts";
+
 @customElement("graph-viz")
 export class GraphViz extends LitElementWw {
   render() {
-    return html`<dfs-graph />`;
+    //return html`<dfs-graph />`;
+    return html`<dijkstra-graph />`;
   }
 }
